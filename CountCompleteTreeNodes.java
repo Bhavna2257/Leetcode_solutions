5bba1f77fc00878fe3af0322f1ae22e7d785dc7b
Bhavna2257 <65300578+Bhavna2257@users.noreply.github.com>
2021-07-08 18:28:45 -0400

    Create CountCompleteTreeNodes.java
    Leetcode 222

---
+++ CountCompleteTreeNodes.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        int noOfNodes = 0;
+        if(root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        while(!queue.isEmpty())
+        {
+            TreeNode curr = queue.remove();
+            //System.out.println("curr="+curr.val);
+            noOfNodes++;
+            if(curr.left != null)
+            {
+                queue.add(curr.left);
+            }
+            if(curr.right != null)
+            {
+                queue.add(curr.right);
+            }
+        }
+        return noOfNodes;
+    }
+}
